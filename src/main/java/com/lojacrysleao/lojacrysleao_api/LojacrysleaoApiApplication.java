11d6e4ca58017fbce6b8983434b9e9924d9f02c2
Hershell Lucas <hershelllucas@gmail.com>
2025-07-23 16:21:39 -0300

    primeiro commit

---
+++ src/main/java/com/lojacrysleao/lojacrysleao_api/LojacrysleaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.lojacrysleao.lojacrysleao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojacrysleaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojacrysleaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojacrysleao/lojacrysleao_api/LojacrysleaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojacrysleao.lojacrysleao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojacrysleaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
