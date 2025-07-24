d495abda255e803cec96f0117b74a49b27faf4ba
Hershell Lucas <hershelllucas@gmail.com>
2025-07-24 20:13:33 -0300

    Implementando JWT

--- src/main/java/com/lojacrysleao/lojacrysleao_api/dto/LoginRequest.java
+++ src/main/java/com/lojacrysleao/lojacrysleao_api/dto/LoginRequest.java
@@ -9,6 +9,14 @@ public class LoginRequest {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getPassword() {
         return password;
     }
