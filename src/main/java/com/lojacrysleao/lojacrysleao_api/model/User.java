23a1005551c6f2a7d18a37ebba04d11a437fcb17
Hershell Lucas <hershelllucas@gmail.com>
2025-07-24 12:23:18 -0300

    Ajustando estrutura

---
+++ src/main/java/com/lojacrysleao/lojacrysleao_api/model/User.java
@@ -0,0 +1,73 @@
+package com.lojacrysleao.lojacrysleao_api.model;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+
+    public String nome;
+
+    public String email;
+
+    public String telefone;
+
+    public String cpf;
+
+    public String password;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/lojacrysleao/lojacrysleao_api/service/ProdutoService.java
@@ -0,0 +1,4 @@
+package com.lojacrysleao.lojacrysleao_api.service;
+
+public class ProdutoService {
+}
