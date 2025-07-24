2d891b0849ee021a3e4e59be4ac8d5c2d249e8b3
Hershell Lucas <hershelllucas@gmail.com>
2025-07-24 18:13:17 -0300

    Comecando a implementar o Backend do Login com JWT

---
+++ src/main/java/com/lojacrysleao/lojacrysleao_api/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.lojacrysleao.lojacrysleao_api.controller;
+
+import com.lojacrysleao.lojacrysleao_api.dto.LoginRequest;
+import com.lojacrysleao.lojacrysleao_api.dto.LoginResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+
+    public AuthController(AuthenticationManager authenticationManager,
+                          JwtTokenProvider tokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        String token = tokenProvider.generateToken(Authentication authentication);
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
+}
--- src/main/java/com/lojacrysleao/lojacrysleao_api/dto/LoginRequest.java
+++ src/main/java/com/lojacrysleao/lojacrysleao_api/dto/LoginRequest.java
@@ -1,4 +1,15 @@
 package com.lojacrysleao.lojacrysleao_api.dto;
 
 public class LoginRequest {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
