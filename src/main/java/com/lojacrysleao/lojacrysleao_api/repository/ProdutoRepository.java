f871a6f4d989fa97965f1fa9990f255fc929f7b3
H. Lucas <151595379+lucasln10@users.noreply.github.com>
2025-07-28 17:22:42 -0300

    Delete src/main/java/com/lojacrysleao/lojacrysleao_api directory
    Signed-off-by: H. Lucas <151595379+lucasln10@users.noreply.github.com>

--- src/main/java/com/lojacrysleao/lojacrysleao_api/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lojacrysleao.lojacrysleao_api.repository;
-
-import com.lojacrysleao.lojacrysleao_api.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
