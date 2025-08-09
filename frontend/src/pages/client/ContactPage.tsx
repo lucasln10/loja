14926098cbd4f3f8e6899b0580e9577769fa90c0
LucasBelhassof <lucasbelhassof@gmail.com>
2025-08-09 23:18:07 +0000

    #72

--- frontend/src/pages/client/ContactPage.tsx
+++ frontend/src/pages/client/ContactPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useCart } from '../context/CartContext';
+import { useCart } from '../../context/CartContext';
 
 const CartPage: React.FC = () => {
   const { state, removeFromCart, updateQuantity, clearCart } = useCart();
